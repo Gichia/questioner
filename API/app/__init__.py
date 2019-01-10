c6c0ea5d46ebdd104d552a54cd2f3d7975efb466
Gichia <petergichia35@gmail.com>
2019-01-10 15:44:22 +0300

    Added a questions blueprint

--- API/app/__init__.py
+++ API/app/__init__.py
@@ -1,6 +1,8 @@
+"""Import Blueprints and create app"""
 from flask import Flask
 from .api.v1.views.user_views import ver1 as v1
 from .api.v1.views.meetup_views import ver1 as v1
+from .api.v1.views.question_views import ver1 as v1
 
 
 def create_app(config):
