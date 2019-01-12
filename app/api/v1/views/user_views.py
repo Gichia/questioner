bab25204e6250d9d1323990a15227041fab5884b
Gichia <petergichia35@gmail.com>
2019-01-12 15:42:19 +0300

    [Starts #163048779] Added a login method to user views

--- app/api/v1/views/user_views.py
+++ app/api/v1/views/user_views.py
@@ -2,12 +2,11 @@
 from flask import Flask, request, jsonify, make_response
 from .. import ver1
 from ... v1.models import users_model
-from werkzeug.security import generate_password_hash, check_password_hash
-import datetime
+from werkzeug.security import generate_password_hash
 from functools import wraps
 
 
-@ver1.route("/auth/register", methods=["POST"])
+@ver1.route("/auth/signup", methods=["POST"])
 def register_user():
     """ Registers a new user"""
 
@@ -18,3 +17,8 @@ def register_user():
     password = generate_password_hash(data['password'], method='sha256')
     new_user = users_model.UserModel().register_user(data["firstname"], data["lastname"], data["email"], data["phone_number"], data["username"], password)
     return jsonify({"status": 201, "message": "You have been successfully registered!", "data": new_user})
+
+
+@ver1.route("/auth/login")
+def login_user():
+    """ Login a user"""
