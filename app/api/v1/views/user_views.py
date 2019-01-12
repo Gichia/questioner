0ac1f5c8cb134f894e8d2d2eacb0415f01ed111c
Gichia <petergichia35@gmail.com>
2019-01-12 08:09:04 +0300

    [Finishes #163047455] Added a endpoint to register new user

--- app/api/v1/views/user_views.py
+++ app/api/v1/views/user_views.py
@@ -1,9 +1,20 @@
-from flask import Flask, request, jsonify
+"""Contains all user endpoints"""
+from flask import Flask, request, jsonify, make_response
 from .. import ver1
+from ... v1.models import users_model
+from werkzeug.security import generate_password_hash, check_password_hash
+import datetime
+from functools import wraps
 
 
-@ver1.route("/", methods=["GET"])
-def hello():
-    """ Returns 'hello world' """
+@ver1.route("/auth/register", methods=["POST"])
+def register_user():
+    """ Registers a new user"""
 
-    return 'Index'
+    data = request.get_json()
+
+    if not data:
+        return jsonify({"status": 400, "message": "Please fill in all fields!"})
+    password = generate_password_hash(data['password'], method='sha256')
+    new_user = users_model.UserModel().register_user(data["firstname"], data["lastname"], data["email"], data["phone_number"], data["username"], password)
+    return jsonify({"status": 201, "message": "You have been successfully registered!", "data": new_user})
