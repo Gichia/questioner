4ffb22ad546bc6e5d98eee42093906ff0368e284
Gichia <petergichia35@gmail.com>
2019-01-13 17:35:56 +0300

    Added validations to user resister endpoint

--- app/api/v1/views/user_views.py
+++ app/api/v1/views/user_views.py
@@ -3,6 +3,10 @@ from werkzeug.security import generate_password_hash
 from flask import Flask, request, jsonify, make_response
 from .. import ver1
 from ... v1.models import users_model
+from .. utils.validations import Validations
+
+
+validate = Validations()
 
 
 @ver1.route("/auth/signup", methods=["POST"])
@@ -13,9 +17,18 @@ def register_user():
 
     if not data:
         return jsonify({"status": 400, "message": "Please fill in all fields!"})
-    password = generate_password_hash(data['password'], method='sha256')
-    new_user = users_model.UserModel().register_user(data["firstname"], data["lastname"], data["email"], data["phone_number"], data["username"], password)
-    return jsonify({"status": 201, "message": "You have been successfully registered!", "data": new_user})
+    elif validate.is_valid_email(data["email"]) == False:
+        return jsonify({"message": "Not a valid email"})
+    elif validate.is_valid_password(data["password"]) == False:
+        return jsonify({"message": "Not a valid password"})
+    elif users_model.UserModel().username_exist(data["username"]) == True:
+        return jsonify({"message": "Username exists!"})
+    elif users_model.UserModel().email_exist(data["email"]) == True:
+        return jsonify({"message": "Email exists!"})
+    else:
+        password = generate_password_hash(data['password'], method='sha256')
+        new_user = users_model.UserModel().register_user(data["firstname"], data["lastname"], data["email"], data["phone_number"], data["username"], password)
+        return jsonify({"status": 201, "message": "You have been successfully registered!", "data": new_user})
 
 
 @ver1.route("/auth/login")
