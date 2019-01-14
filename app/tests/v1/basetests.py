f8d284f0b61aa3f38588dde503882c3f356fb4c5
Gichia <petergichia35@gmail.com>
2019-01-14 22:50:02 +0300

    Added base tests for all tests files

--- app/tests/v1/basetests.py
+++ app/tests/v1/basetests.py
@@ -4,8 +4,11 @@ import json
 from app import create_app
 
 meetup_url = 'http://localhost:5000/api/v1/meetups'
+upcoming = 'http://localhost:5000/api/v1/meetups/upcoming'
+rsvp_url = 'http://localhost:5000/api/v1/meetups/rsvp/1'
 post_question = 'http://localhost:5000/api/v1/questions/1'
 specific_meetup = 'http://localhost:5000/api/v1/meetups/1'
+meetup_questions = 'http://localhost:5000/api/v1/questions/1'
 
 
 class BaseTest(unittest.TestCase):
@@ -25,22 +28,39 @@ class BaseTest(unittest.TestCase):
             "title": "Question One", 
             "body": "Suggested question One"
         }
+        self.rsvp = {
+            "status": "maybe"
+        }
 
 
     def post_meetup(self):
         """Create a valid post for test purposes"""
         return self.client.post(meetup_url, data=json.dumps(self.meetup), content_type="application/json")
 
+    def get_meetups(self):
+        """Get all meetup"""
+        return self.client.get(meetup_url)
+
+    def get_upcoming(self):
+        """Get upcoming meetups"""
+        return self.client.get(upcoming)
+
+    def post_rsvp(self):
+        """Post rsvp for testing"""
+        return self.client.post(rsvp_url, data=json.dumps(self.rsvp), content_type="application/json")
     
     def get_single_meetup(self):
         """Get specific meetup"""
         return self.client.get(specific_meetup)
 
-    
     def post_question(self):
+        """Post a question for test purposes"""
         return self.client.post(post_question, data=json.dumps(self.question), content_type="application/json")
 
-    
+    def meetup_questions(self):
+        """Get specific meetup questions for test purposes"""
+        return self.client.get(meetup_questions)
+
     def return_json(self, response):
         """Return json formated response for test purpose"""
         return json.loads(response.data.decode("UTF-8"))
