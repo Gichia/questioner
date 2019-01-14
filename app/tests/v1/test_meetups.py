3e356bf56b18c0de8222baafb63afa87463978ad
Gichia <petergichia35@gmail.com>
2019-01-14 22:51:13 +0300

    Added more tests for meetup endpoints

--- app/tests/v1/test_meetups.py
+++ app/tests/v1/test_meetups.py
@@ -8,14 +8,23 @@ specific_meetups = 'http://localhost:5000/api/v1/meetups/1'
 class TestMeetups(BaseTest):
     """ Class to test all meetup endpoints """
 
-
-    def test_get_all_meetups(self):
+    def test_post_meetup(self):
         """Method to test get all meetups endpoint"""
         response = self.post_meetup()
         result = self.return_json(response)
 
         self.assertEqual(result["status"], 201)
         self.assertEqual(result["message"], "New meetup created successfully!")
+        self.assertEqual(response.status, "200 OK")
+        self.assertTrue(response.content_type == "application/json")
+
+    def test_get_all_meetups(self):
+        """A test for the get all meetups endpoint"""
+        self.post_meetup()
+        response = self.get_meetups()
+        result = self.return_json(response)
+
+        self.assertEqual(result["status"], 200)
         self.assertEqual(response.status, "200 OK")
         self.assertTrue(response.content_type == "application/json")
 
@@ -29,3 +38,27 @@ class TestMeetups(BaseTest):
         self.assertEqual(results.status_code, 200)
         self.assertEqual(results.status, "200 OK")
         self.assertTrue(results.content_type == "application/json")
+    
+    def test_get_upcoming(self):
+        """Test for get upcoming meetups endpoint"""
+        self.post_meetup()
+        response = self.get_upcoming()
+        result = self.return_json(response)
+
+        self.assertEqual(result["status"], 200)
+        self.assertEqual(response.status, "200 OK")
+        self.assertTrue(response.content_type == "application/json")
+
+    def test_rsvp(self):
+        """Test respond to meetup rsvp"""
+        self.post_meetup()
+        response = self.post_rsvp()
+        result = self.return_json(response)
+
+        self.assertIn("Rsvp responded to!", result["message"])
+        self.assertEqual(result["status_code"], 201)
+        self.assertEqual(response.status, "200 OK")
+        self.assertTrue(response.content_type == "application/json")
+
+
+
