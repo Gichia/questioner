6938e7c31e61b3e94976cfcddd58a93dad291d4a
Gichia <petergichia35@gmail.com>
2019-01-17 09:59:09 +0300

    Added a signup endpoint for version 2

---
+++ app/api/v2/views/user_views.py
@@ -0,0 +1,9 @@
+"""User endpoints"""
+from flask import request, jsonify
+from app.api.v2 import ver2
+
+
+@ver2.route('/auth/signup')
+def user_signup():
+    """Register new user endpoint"""
+    return 'Away'
