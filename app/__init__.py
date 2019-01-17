c9684ce9322ac216d34bc338ec413bba68e91911
Gichia <petergichia35@gmail.com>
2019-01-17 09:49:45 +0300

    [Starts #163250089] Added blue prints for version 2 of the api

--- app/__init__.py
+++ app/__init__.py
@@ -2,10 +2,12 @@ from flask import Flask
 from .api.v1.views.user_views import ver1 as v1
 from .api.v1.views.meetup_views import ver1 as v1
 from .api.v1.views.question_views import ver1 as v1
+from .api.v2.views.user_views import ver2 as v2
 
 
 def create_app(config):
     '''function creating the flask app'''
     app = Flask(__name__)
     app.register_blueprint(v1)
+    app.register_blueprint(v2)
     return app
