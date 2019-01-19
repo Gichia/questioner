14aa282908ddf58ba64754590812f496f156cb8f
Gichia <petergichia35@gmail.com>
2019-01-19 19:05:50 +0300

    Added an endpoint to save new user details

--- app/api/v2/views/user_views.py
+++ app/api/v2/views/user_views.py
@@ -1,9 +1,26 @@
 """User endpoints"""
-from flask import request, jsonify
+from flask import request, jsonify, make_response
+from app.api.v2.models.users_model import UserClass
 from app.api.v2 import ver2
 
+db = UserClass()
 
-@ver2.route('/auth/signup')
+@ver2.route("/auth/signup", methods=["POST"])
 def user_signup():
     """Register new user endpoint"""
-    return 'Home'
+    try:
+        data = request.get_json()
+        firstname = data["firstname"]
+        lastname = data["lastname"]
+        email = data["email"]
+        password = data["password"]
+    except:
+        return make_response(jsonify({
+            "status": 500,
+            "message": "Please provide correct details"
+        }))
+
+    # Validations
+    phone = data["phone"]
+    db.save_user(firstname, lastname, email, password, phone)
+    return 'True'
