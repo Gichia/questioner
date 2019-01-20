7c2e5b4355cb6912ad4ae897222f31615655a58a
Gichia <petergichia35@gmail.com>
2019-01-20 21:50:49 +0300

    [Starts #163347934] Registered an error handler

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from .api.v2.utils.errors import bad_request, internal_server_error, not_found
 from .api.v1.views.user_views import ver1 as v1
 from .api.v1.views.meetup_views import ver1 as v1
 from .api.v1.views.question_views import ver1 as v1
@@ -11,4 +12,7 @@ def create_app(config):
     app = Flask(__name__)
     app.register_blueprint(v1)
     app.register_blueprint(v2)
+    app.register_error_handler(404, not_found)
+    app.register_error_handler(405, bad_request)
+    app.register_error_handler(500, internal_server_error)
     return app
